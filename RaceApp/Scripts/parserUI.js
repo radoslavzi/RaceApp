60dce015a55d334451401120db4d067aa6d6195e
Radoslav Ivanov <radoslav.zlatev@gmail.com>
2014-09-27 19:02:13 +0300

    Race App Commit
    SB tech task interview

---
+++ RaceApp/Scripts/parserUI.js
@@ -0,0 +1,65 @@
+﻿var uiParser = (function () {
+    function Parser(url) {
+        this.url = url;
+    }
+
+    Parser.prototype = {
+        getData: function (url) {
+            debugger
+            var url = url;
+            var self = this;
+            $.get(url, function (data) {
+                
+                self.renderUI(JSON.parse(data.Data));
+            });
+        },
+        renderUI: function (data) {
+            var div = '';
+            for (var i in data) {
+                div += "<div class='event'><div>Race Event Object</div>" +
+                "<div>Id: " + data[i].RaceEvent.Id + "</div>" +
+                "<div>EventNumber: " + data[i].RaceEvent.EventNumber + "</div>" +
+                "<div>FinishTime: " + data[i].RaceEvent.FinishTime + "</div>" +
+                "<div>Distance: " + data[i].RaceEvent.Distance + "</div>" +
+                 "<div>Name: " + data[i].RaceEvent.Name + "</div>";
+                
+                var entries = data[i].RaceEventEntries;
+                div +="<div class='entire-events'>"
+                for (var j in entries) {
+                    div+="<div class='entire-event'>"
+                    div += "<div>Id: " + entries[j].Id + "</div>" +
+                         "<div>Name: " + entries[j].Name + "</div>" +
+                          "<div>OddsDecimal: " + entries[j].OddsDecimal + "</div>";
+                    div += '</div>';
+                }
+                div += "</div></div>";
+
+                $('#parse-data').append(div);
+            }
+            this.listeners();
+        },
+        listeners: function () {
+            var self = this;
+            $('#sort-asc').click(function (ev) {
+                ev.preventDefault();
+                $('#parse-data').html('');
+                self.getData('/api/parse/get?isAsc=' + true);
+            })
+
+            $('#sort-desc').click(function (ev) {
+                ev.preventDefault();
+                $('#parse-data').html('');
+                self.getData('/api/parse/get?isDesc=' + true);
+            })
+        }
+    }
+
+    return {
+        getParser: function (url) {
+            return new Parser(url);
+        }
+    }
+})();
+
+var parser = uiParser.getParser('/api/parse');
+parser.getData('/api/parse');
